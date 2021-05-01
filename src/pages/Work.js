ed6d263325d4dd901103d15434909303e1098904
haywah27 <hayleywahlroos15@gmail.com>
2021-04-30 17:03:19 -0700

    adding projects to work page

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./work.css";
 import { Container, Row } from "react-bootstrap";
 import Navigation from "../components/Nav/Nav";
+
+// project images
 import WorkCard from "../components/WorkCard/WorkCard";
-import moodring from "../assets/mood-ring.png";
+import moodring from "../assets/Projects/mood-ring.png";
+import crypto from "../assets/Projects/crypto.png";
+import musicSpider from "../assets/Projects/music-spider.png";
+import burgerWorks from "../assets/Projects/burgerWorks.png";
+import weatherDash from "../assets/Projects/weather-dash.png";
+import employeeDirectory from "../assets/Projects/employeeDirectory.png";
 
 function Work() {
   return (
@@ -18,6 +25,42 @@ function Work() {
             githubLink="https://github.com/haywah27/mood-ring"
             liveWebpage="https://moody-ring.herokuapp.com/"
           />
+          <WorkCard
+            screenshot={crypto}
+            title="Crypto Investment Tracker"
+            content=""
+            githubLink="https://github.com/team-crypto-currency/crypto-investment-tracker"
+            liveWebpage="https://crypto-investment-tracker.herokuapp.com/"
+          />
+          <WorkCard
+            screenshot={musicSpider}
+            title="Music Spider"
+            content=""
+            githubLink="https://github.com/haywah27/Music-Spider"
+            liveWebpage="https://haywah27.github.io/Music-Spider/"
+          />
+          <WorkCard
+            screenshot={burgerWorks}
+            title="BurgerWorks"
+            content=""
+            githubLink=""
+            liveWebpage=""
+          />
+          <WorkCard
+            screenshot={employeeDirectory}
+            title="Employee Directory"
+            content=""
+            githubLink="https://github.com/haywah27/employee-directory"
+            liveWebpage="https://haywah27.github.io/employee-directory/"
+          />
+          <WorkCard
+            screenshot={weatherDash}
+            title="Weather Dashboard"
+            content=""
+            githubLink=""
+            liveWebpage=""
+          />
+          
         </Row>
       </Container>
       <Container className="pageFiller"></Container>
