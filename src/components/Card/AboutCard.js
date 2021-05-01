b71653b0900367d9e3b14fe565b6d0de1a905fd6
haywah27 <hayleywahlroos15@gmail.com>
2021-04-30 22:42:33 -0700

    media query updates on about page

--- src/components/Card/AboutCard.js
+++ src/components/Card/AboutCard.js
@@ -6,10 +6,10 @@ function AboutCard() {
   return (
     <Card className="cardCss">
       <Card.Body className="aboutBody">
-        I graduated from the University of Oregon with a Bachelor of Science
-        Degree in Art and Technology. Recently, I have shifted my focus from
-        design to full stack development. I love dogs, learning new things and
-        exploring the outdoors!
+        I graduated from the University of Oregon with a Bachelor of Science in
+        Art and Technology. Recently, I have shifted my focus from design to
+        full stack development. I love dogs, learning new things and exploring
+        the outdoors!
       </Card.Body>
     </Card>
   );
