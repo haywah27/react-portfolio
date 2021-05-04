cc9bd335d728e98ecd38024e73e26d78643dd461
haywah27 <hayleywahlroos15@gmail.com>
2021-05-04 15:02:15 -0700

    changing routes to match gh-pages rendering

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,20 +5,20 @@ import { Navbar, Nav } from "react-bootstrap";
 function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" className="nav" variant="dark">
-      <Navbar.Brand href="/" className="navbar-header">
+      <Navbar.Brand href="/react-portfolio/" className="navbar-header">
         Hayley Wahlroos
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav" bg="dark" variant="dark">
         <Nav className="mr-auto"></Nav>
         <Nav>
-          <Nav.Link className="navbar-link" href="/">
+          <Nav.Link className="navbar-link" href="/react-portfolio/">
             About
           </Nav.Link>
-          <Nav.Link className="navbar-link" href="/work">
+          <Nav.Link className="navbar-link" href="/react-portfolio/work">
             Work
           </Nav.Link>
-          <Nav.Link className="navbar-link" href="/contact">
+          <Nav.Link className="navbar-link" href="/react-portfolio/contact">
             Contact
           </Nav.Link>
         </Nav>
